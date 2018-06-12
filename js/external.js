0b9577917584ad4a70d740d38c3ffc4d1b599783
fer <fernandoza@gmail.com>
2018-06-12 15:32:13 -0500

    feat: add the rest of the lecture for HTML + JS

---
+++ js/external.js
@@ -0,0 +1,18 @@
+'use strict';
+
+console.log('This is my external file loaded');
+
+console.log(1 + 2);
+
+console.error('Something went wrong');
+
+var age = prompt('How old are you?');
+
+console.log(age);
+
+// alert('Your age is ' + age);
+
+var answer = confirm('Is it your bday today?');
+console.log(answer);
+
+
