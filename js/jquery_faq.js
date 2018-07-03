abf17290437ffad337e3645ee3d28a968bd7d8a7
Justin Reich <justinreich.dev@gmail.com>
2018-07-03 11:56:24 -0500

    Solutions and bonus solutions for jQuery Traversal exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,8 +1,65 @@
-$("#details").click(function() {
-    // event.preventDefault();
-    $("dd").toggleClass("invisible");
+$(document).ready(function() {
+    "use strict";
+
+
+    // ATTRIBUTES EXERCISE
+    $("#details").click(function() {
+        // event.preventDefault();
+        $("dd").toggleClass("invisible");
+    });
+
+    $("dt").click(function() {
+        $(this).toggleClass("highlighted");
+    });
+
+
+    // TRAVERSAL EXERCISE
+    // $('ul').each(function() {
+    //     $(this).children().last().css('background', 'yellow');
+    // });
+
+    // $('ul').each(function(index) {
+    //     $($('ul')[index]).children().last().css('background', 'yellow');
+    // });
+
+    // problem 3
+    function highLightBtn() {
+        $('ul').each(function() {
+            $(this).children().last().css('background', 'yellow');
+        });
+    }
+
+    $('#highlightBtn').click(highLightBtn);
+
+
+    // problem 4
+    $('h3').click(function() {
+        $(this).next().css('font-weight', 'bold');
+    });
+
+    $('li').click(function() {
+        $(this).parent().children().first().css('color', 'blue');
+    });
+
+
+
+
+
 });
 
-$("dt").click(function() {
-    $(this).toggleClass("highlighted");
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
